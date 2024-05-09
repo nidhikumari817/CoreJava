c2bebd789943feac967cab23c1504a91e3227892
atul anand <37778673+atul161@users.noreply.github.com>
2024-05-09 21:43:58 +0530

    Update Armstrong.java

--- src/Armstrong.java
+++ src/Armstrong.java
@@ -1,6 +1,16 @@
 import java.lang.Math;
 
 public class Armstrong {
+
+	public int getOrder(int number) {
+            int order = 0 ;		
+            while(number > 0) {
+	        // Getting the Remainder Example: 124 %10 = 4
+	        number = number / 10;
+		order++;    
+	    }
+	  return order;	
+	}		
     	public void isArmStrong(int num) {
 	    if (num < 0){
 	        System.out.println("Negative Number can not be Armstrong");
@@ -8,10 +18,11 @@ public class Armstrong {
 	    }
 	    int sum = 0;
 	    int meta = num;
+            int order = getOrder(meta);		
 	    while(meta > 0) {
 	        // Getting the Remainder Example: 124 %10 = 4
 	        int remainder = meta % 10;
-	        sum = sum +  (remainder * remainder * remainder);
+	        sum = sum +  Math.pow(remainder,order);
 	        meta = meta / 10;
 	    }
 	    if (sum == num){
